424b4a3422f9d7440cb3c1198d351920c3216239
Paul_BERGEON <102290558+PaulWNB@users.noreply.github.com>
2023-05-19 14:48:22 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -9,7 +9,7 @@ from keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt
 
 # Chemin d'accès au fichier CSV
-filename = '/Users/paulbergeon/Documents/barique/Classeur10.csv'
+filename = '/Users/paulbergeon/Documents/mémoireESSCA/Classeur10.csv'
 
 # Lecture du fichier CSV et conversion de la colonne Date en format datetime
 with open(filename, 'rb') as f:
