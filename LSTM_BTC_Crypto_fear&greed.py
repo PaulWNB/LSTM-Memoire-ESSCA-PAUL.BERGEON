90395cb23d998417240d17de8611a37ab519a8d2
Paul_BERGEON <102290558+PaulWNB@users.noreply.github.com>
2023-05-19 14:44:26 +0200

    Update and rename LSTM4.py to LSTM_BTC_Crypto_fear&greed.py

--- LSTM_BTC_Crypto_fear&greed.py
+++ LSTM_BTC_Crypto_fear&greed.py
@@ -9,7 +9,7 @@ from keras.layers import LSTM, Dense
 import matplotlib.pyplot as plt
 
 # Chemin d'accès au fichier CSV
-filename = '/Users/paulbergeon/Documents/barique/Sentiment:PrixBTC.csv'
+filename = '/Users/paulbergeon/Documents/mémoireESSCA/Sentiment:PrixBTC.csv'
 
 # Lecture du fichier CSV et conversion de la colonne Date en format datetime
 with open(filename, 'rb') as f:
